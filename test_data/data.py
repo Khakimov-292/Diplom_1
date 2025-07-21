04462dec2554050292d330c3a730880f39fc5915
Khakimov-292 <kyubi292@yandex.ru>
2025-07-21 16:46:39 +0300

    Добавил рецепт в data.py

--- test_data/data.py
+++ test_data/data.py
@@ -6,3 +6,4 @@ INGREDIENT_SAUCE_PRICE = 100
 INGREDIENT_FILLING_NAME = 'Laimon'
 INGREDIENT_FILLING_TYPE = 'FILLING'
 INGREDIENT_FILLING_PRICE = 300
+RECEIPT = '(==== Флюоресцентная булка ====)\n= filling Laimon =\n= sauce sauce =\n(==== Флюоресцентная булка ====)\n\nPrice: 600'
